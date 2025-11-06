c92c8aa53baff8fcaa24fe93d67c5f05a68963f5
Nguyen Tuan Dat <nguyentuandat1110@gmail.com>
2025-11-06 13:32:08 +0700

    init fe project

---
+++ FE/src/App.tsx
@@ -0,0 +1,37 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import HomePage from "./pages/home-page";
+import LoginPage from "./pages/login-page";
+import RegisterPage from "./pages/register-page";
+import Dashboard from "./pages/dashboard";
+import UsersPage from "./pages/users-page";
+import SettingPage from "./pages/setting-page";
+import ProtectedRoute from "./components/protected-route";
+import ForgotPasswordPage from "./pages/forgot-password-page";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to ="/login" />} />
+        <Route 
+          path="/dashboard" 
+          element={
+            //<ProtectedRoute>
+              <Dashboard />
+            //</ProtectedRoute>
+          } 
+        >
+          <Route path="home" element={<HomePage />} />
+          <Route path="users" element={<UsersPage />} />
+          <Route path="settings" element={<SettingPage />} />
+        </Route>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        {/* <Route path="*" element={<Navigate to="/login" />} /> */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
