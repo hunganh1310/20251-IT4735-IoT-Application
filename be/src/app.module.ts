5e71e63d65aaeee5cbbbf295e4edf14b8a32861f
Nguyen Tuan Dat <nguyentuandat1110@gmail.com>
2025-12-08 21:56:38 +0700

    update

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -8,14 +8,20 @@ import { WebsocketGateway } from './websocket/websocket.gateway';
 import { AuthModule } from './auth/auth.module';
 import { WebsocketModule } from './websocket/websocket.module';
 import { MqttModule } from './mqtt/mqtt.module';
+import { InfluxModule } from './influx/influx.module';
+import { DeviceModule } from './device/device.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 
 @Module({
   imports: [
+    EventEmitterModule.forRoot(),
     AccountModule, 
     PrismaModule,  
     AuthModule, 
     WebsocketModule,
     MqttModule,
+    InfluxModule,
+    DeviceModule,
   ], 
   controllers: [AppController],
   providers: [AppService, MqttService, WebsocketGateway],
